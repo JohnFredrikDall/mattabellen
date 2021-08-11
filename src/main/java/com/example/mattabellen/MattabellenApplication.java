c382e5210dfad96a13ed91304f28ee4695994acf
JohnFredrikDall <joffe.dall@gmail.com>
2021-08-11 09:22:48 +0200

    First!

---
+++ src/main/java/com/example/mattabellen/MattabellenApplication.java
@@ -0,0 +1,13 @@
+package com.example.mattabellen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattabellenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MattabellenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mattabellen/MattabellenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mattabellen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MattabellenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
